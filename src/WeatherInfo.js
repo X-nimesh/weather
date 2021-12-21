110f883b7c329903bccbcfa210b1a691fee08d09
X-nimesh <nimush14@gmail.com>
2021-12-21 17:35:18 +0545

    styling left

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,40 +6,42 @@ import { useEffect, useState } from "react";
 
 function WeatherInfo(){
     
-    let day="sunday";
+    
     const d = new Date();
-    let dayN = d.getDay();
-    
+    // let dayN = d.getDay();
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var date = (new Date()).toString().split(' ').splice(1,2).join(' ');
-    
-    if(dayN==0)
-    {
-        day="Sunday";
-    }
-    if(dayN==1)
-    {
-        day="Monday";
-    }
-    if(dayN==2)
-    {
-        day="Tuesday";
-    }
-    if(dayN==3)
-    {
-        day="Wednesday";
-    }
-    if(dayN==4)
-    {
-        day="Thursday";
-    }
-    if(dayN==5)
-    {
-        day="Friday";
-    }
-    if(dayN==6)
-    {
-        day="Saturday";
-    }
+    // let time=d.getHours()+":"+d.getMinutes();
+    let time= d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    let day= days[d.getDay()];
+    // if(dayN==0)
+    // {
+    //     day="Sunday";
+    // }
+    // if(dayN==1)
+    // {
+    //     day="Monday";
+    // }
+    // if(dayN==2)
+    // {
+    //     day="Tuesday";
+    // }
+    // if(dayN==3)
+    // {
+    //     day="Wednesday";
+    // }
+    // if(dayN==4)
+    // {
+    //     day="Thursday";
+    // }
+    // if(dayN==5)
+    // {
+    //     day="Friday";
+    // }
+    // if(dayN==6)
+    // {
+    //     day="Saturday";
+    // }
     
     
     window.navigator.geolocation.getCurrentPosition(console.log, console.log);
@@ -49,7 +51,7 @@ function WeatherInfo(){
     let [weather,setWeather]=useState([]);
     let [temp,setTemp]=useState([]);
     let [geo,setlocation]=useState(["kathmandu"]);
-    var weather1;
+   
     
     function readValue(value)
     {
@@ -71,8 +73,7 @@ function WeatherInfo(){
         })
         .then((data)=>{
             console.log(data);
-            // weather1=data;
-            // console.log(weather1);
+            
             setWeather(data.weather[0]);
             // console.log(weather);
             
@@ -88,6 +89,9 @@ function WeatherInfo(){
     console.log(weather);
     console.log(temp);
 
+    let icon="https://openweathermap.org/img/wn/"+weather.icon+"@4x.png";
+
+
     return(
         <div className="main">
            
@@ -99,26 +103,47 @@ function WeatherInfo(){
                     Current Weather
             </p>
             <div  className="info">
+                <div className="iconcity">
+                    <div className="cityTime">
+                        <p className="city">
+                        {geo}
+                        </p>
+                        <p className="time">
+                            {time},{day},{date}
+                        </p>
+                    </div>
+                    <img src={icon} alt="" className="weathericon" />
+                </div>
                 <div className="basic">
-                    <p className="day">
-                    {day}
+                    {/* <p className="day">
+                    
                     </p>
                     <p className="date">
-                        {date}
-                    </p>
-                    <p className="city">
-                    {geo}
-                    </p>
-                    <p className="temperature">
-                        {temp.temp}­°C
-                    </p>
+                        
+                    </p> */}
+                    
+                    <div className="temperature">
+                        <p className="temp">{temp.temp}­ </p> <p className="cel1">°C</p> 
+                    </div>
                     <p className="condition">
                         {weather.description}
                         
                     </p>
-                </div>
-                <div className="detail"></div>
-            
+                <div className="desc">
+                    <div className="humidity">
+                        <p className="hum">{temp.humidity}%</p>
+                        <p className="humt">Humidity</p>
+                    </div>
+                    <div className="maxtemp">
+                        <p className="temM">{temp.temp_max}°C</p>
+                        <p className="tempt">Max</p>
+                    </div>
+                    <div className="mintemp">
+                       <p className="minN"> {temp.temp_min}°C</p>
+                       <p className="mint">Min</p>
+                    </div>
+                    </div>
+                </div>           
             </div>
         </div>
     )
